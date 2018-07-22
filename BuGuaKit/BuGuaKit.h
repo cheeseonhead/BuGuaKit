616fbec7a20baef4b4a8b66a56e13eda163604d9
Jeff Woo <cheeseonhead@gmail.com>
2018-07-21 21:07:00 -0400

    Initial Commit

---
+++ BuGuaKit/BuGuaKit.h
@@ -0,0 +1,19 @@
+//
+//  BuGuaKit.h
+//  BuGuaKit
+//
+//  Created by Cheese Onhead on 2018-07-21.
+//  Copyright © 2018 cheeseonhead. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BuGuaKit.
+FOUNDATION_EXPORT double BuGuaKitVersionNumber;
+
+//! Project version string for BuGuaKit.
+FOUNDATION_EXPORT const unsigned char BuGuaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BuGuaKit/PublicHeader.h>
+
+
