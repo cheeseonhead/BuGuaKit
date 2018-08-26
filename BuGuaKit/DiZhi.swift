0a551505b744e9c13f56c945d95e96e2364b96a9
Jeffrey Wu <jwu@touchbistro.com>
2018-08-25 21:39:31 -0400

    Added dizhi file

---
+++ BuGuaKit/DiZhi.swift
@@ -0,0 +1,13 @@
+//
+//  DiZhi.swift
+//  BuGuaKit
+//
+//  Created by Jeffrey Wu on 2018-08-25.
+//  Copyright © 2018 cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+public enum DiZhi {
+    case zi, chou, yin, mao, chen, si, wu, wei, shen, you, xu, hai
+}
