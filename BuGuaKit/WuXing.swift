367484b1b8223daeb313785b448890d194b22ce7
Jeffrey Wu <jwu@touchbistro.com>
2018-08-25 21:24:56 -0400

    Added generates and overcome

--- BuGuaKit/WuXing.swift
+++ BuGuaKit/WuXing.swift
@@ -8,6 +8,34 @@
 
 import Foundation
 
-public enum WuXing {
+public enum WuXing: CaseIterable {
     case gold, wood, water, fire, earth
+
+    var generates: WuXing {
+        switch self {
+        case .gold: return .water
+        case .water: return .wood
+        case .wood: return .fire
+        case .fire: return .earth
+        case .earth: return .gold
+        }
+    }
+
+    var generatedBy: WuXing {
+        return WuXing.allCases.first(where: { $0.generates == self })!
+    }
+
+    var overcomes: WuXing {
+        switch self {
+        case .gold: return .wood
+        case .wood: return .earth
+        case .earth: return .water
+        case .water: return .fire
+        case .fire: return .gold
+        }
+    }
+
+    var overcomedBy: WuXing {
+        return WuXing.allCases.first(where: { $0.overcomes == self })!
+    }
 }
