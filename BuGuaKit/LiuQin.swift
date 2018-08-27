28e0dad0c01bd4e8f3313ba502bb1bafff7a8276
Jeffrey Wu <jwu@touchbistro.com>
2018-08-27 12:22:03 -0400

    Added 六親

---
+++ BuGuaKit/LiuQin.swift
@@ -0,0 +1,13 @@
+//
+//  LiuQin.swift
+//  BuGuaKit
+//
+//  Created by Jeffrey Wu on 2018-08-27.
+//  Copyright © 2018 cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+enum LiuQin {
+    case parent, descendant, wealth, sibling, superior
+}
