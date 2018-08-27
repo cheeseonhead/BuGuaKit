28390c441190be2ba2f83df7c450999b80cfd344
Jeffrey Wu <jwu@touchbistro.com>
2018-08-27 12:24:42 -0400

    Added the 五行 for 地支

--- BuGuaKit/DiZhi.swift
+++ BuGuaKit/DiZhi.swift
@@ -11,5 +11,13 @@ import Foundation
 public enum DiZhi {
     case zi, chou, yin, mao, chen, si, wu, wei, shen, you, xu, hai
 
-    
+    var wuXing: WuXing {
+        switch self {
+        case .chou, .chen, .wei, .xu: return .earth
+        case .hai, .zi: return .water
+        case .yin, .mao: return .wood
+        case .si, .wu: return .fire
+        case .shen, .you: return .gold
+        }
+    }
 }
