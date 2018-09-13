671ee6c8a213ad570b6e64d19134916f1745892b
Jeff Woo <cheeseonhead@gmail.com>
2018-09-12 22:46:36 -0400

    Added a fuShen controller

---
+++ Sources/BuGuaKit/FuShenController.swift
@@ -0,0 +1,51 @@
+//
+//  FuShenController.swift
+//  BuGuaKit
+//
+//  Created by Cheese Onhead on 2018-09-12.
+//  Copyright © 2018 cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+public class FuShenController {
+    
+    private let guaXiang: LiuYaoGuaXiang
+    
+    // MARK: - Private
+    private lazy var pureLiuShiSiGua: LiuShiSiGua = LiuShiSiGua(innerGua: guaXiang.originalGua.guaGong, outerGua: guaXiang.originalGua.guaGong)
+    
+    public init(guaXiang: LiuYaoGuaXiang) {
+        self.guaXiang = guaXiang
+    }
+    
+    public func fuShen() -> [LiuQin?] {
+        let originalLiuQin = guaXiang.originalGua.liuQin
+        let pure = pureLiuQin()
+        
+        return pure.map { originalLiuQin.contains($0) ? nil : $0 }
+    }
+    
+    public func hiddenTianGan() -> [TianGan?] {
+        let pureTianGan = pureLiuShiSiGua.tianGan
+        
+        return zip(fuShen(), pureTianGan).map { shen, gan in
+            return shen != nil ? gan : nil
+        }
+    }
+    
+    public func hiddenDiZhi() -> [DiZhi?] {
+        let pureDiZhi = pureLiuShiSiGua.yaoZhi
+        
+        return zip(fuShen(), pureDiZhi).map { shen, zhi in
+            return shen != nil ? zhi : nil
+        }
+    }
+}
+
+private extension FuShenController {
+    
+    func pureLiuQin() -> [LiuQin] {
+        return pureLiuShiSiGua.liuQin
+    }
+}
