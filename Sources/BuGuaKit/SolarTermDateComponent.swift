0ce942ed0efe9f0448879baf5978f0a5bcfc8b26
Jeffrey Wu <jwu@touchbistro.com>
2018-09-16 06:59:40 +0800

    Added a quick sanity test to check that it's grabbing the right index

--- Sources/BuGuaKit/SolarTermDateComponent.swift
+++ Sources/BuGuaKit/SolarTermDateComponent.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+class SolarTermDateComponent {
+
+    enum Error: LocalizedError {
+        case yearOutOfRange(Int, Int, Int)
+
+        var errorDescription: String? {
+            switch self {
+            case let .yearOutOfRange(year, min, max):
+                return "\(year)不介於\(min)與\(max)之間"
+            }
+        }
+    }
+
+    let referenceTimeZone = TimeZone.taipei
+    private let minYear = 2000
+    private let maxYear = 2099
+    private let dataLoader: SolarTermDataLoader
+
+    init(dataLoader: SolarTermDataLoader) {
+        self.dataLoader = dataLoader
+    }
+
+    func dateComponent(for term: SolarTerm, year: Int) throws -> DateComponents {
+        guard year >= 2000 && year <= 2099 else {
+            throw Error.yearOutOfRange(year, minYear, maxYear)
+        }
+
+        let startOfYearIndex = (year - minYear) * 24
+        let termIndex = startOfYearIndex + term.index
+
+        let dateData = try dataLoader.loadData()[termIndex]
+
+        return DateComponents(timeZone: referenceTimeZone,
+                              year: dateData[0],
+                              month: dateData[1],
+                              day: dateData[2],
+                              hour: dateData[3],
+                              minute: dateData[4],
+                              second: dateData[5])
+    }
+}
--- Tests/BuGuaKitTests/SolarTermDateComponentTests.swift
+++ Tests/BuGuaKitTests/SolarTermDateComponentTests.swift
@@ -19,7 +19,15 @@ class SolarTermDateComponentTests: XCTestCase {
     }
 
     func testExample() {
-//        try! sut.dateComponent(for: .chunFen, year: 2084)
+        let components = try! sut.dateComponent(for: .chunFen, year: 2084)
+
+        XCTAssertEqual(components.timeZone, .taipei)
+        XCTAssertEqual(components.year, 2084)
+        XCTAssertEqual(components.month, 3)
+        XCTAssertEqual(components.day, 19)
+        XCTAssertEqual(components.hour, 23)
+        XCTAssertEqual(components.minute, 58)
+        XCTAssertEqual(components.second, 51)
     }
 }
 
@@ -28,9 +36,13 @@ private class MockDataLoader: SolarTermDataLoader {
     let data: [[Int]]
 
     override init() {
-        data = (1...2400).map { _ in
+        var data = (1...2400).map { _ in
             [1, 2, 3, 4, 5]
         }
+
+        data[2021] = [2084, 3, 19, 23, 58, 51]
+
+        self.data = data
     }
 
     override func loadData() throws -> [[Int]] {
