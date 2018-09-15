3bc8c92c05d55ea747797d72acb747e571d0346c
Jeffrey Wu <jwu@touchbistro.com>
2018-09-16 06:53:07 +0800

    Prepare the tests to run without the actual data loader

--- Tests/BuGuaKitTests/SolarTermDateComponentTests.swift
+++ Tests/BuGuaKitTests/SolarTermDateComponentTests.swift
@@ -7,27 +7,33 @@
 //
 
 import XCTest
+@testable import BuGuaKit
 
 class SolarTermDateComponentTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var sut: SolarTermDateComponent!
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func setUp() {
+        let fakeDataLoader = MockDataLoader()
+        sut = SolarTermDateComponent(dataLoader: fakeDataLoader)
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//        try! sut.dateComponent(for: .chunFen, year: 2084)
     }
+}
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+private class MockDataLoader: SolarTermDataLoader {
+
+    let data: [[Int]]
+
+    override init() {
+        data = (1...2400).map { _ in
+            [1, 2, 3, 4, 5]
         }
     }
 
+    override func loadData() throws -> [[Int]] {
+        return data
+    }
 }
