8ff4d0f385ee065360a0b561c45cd792a678586b
Jeffrey Wu <jwu@touchbistro.com>
2018-09-30 17:55:21 -0400

    Added diZhi for gregorianTime

--- Sources/BuGuaKit/GregorianTime.swift
+++ Sources/BuGuaKit/GregorianTime.swift
@@ -31,4 +31,23 @@ public struct GregorianTime {
     public var components: DateComponents {
         return DateComponents(hour: hour, minute: minute, second: 0)
     }
+
+    public var diZhi: DiZhi {
+        switch hour {
+        case 0: return .zi
+        case 1...2: return .chou
+        case 3...4: return .yin
+        case 5...6: return .mao
+        case 7...8: return .chen
+        case 9...10: return .si
+        case 11...12: return .wu
+        case 13...14: return .wei
+        case 15...16: return .shen
+        case 17...18: return .you
+        case 19...20: return .xu
+        case 21...22: return .hai
+        case 23...: return .zi
+        default: fatalError()
+        }
+    }
 }
